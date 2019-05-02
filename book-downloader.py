d808e1ccb673c8ca818b4a88e35cf4710c520669
Kawser Habib <kawser.habib.dev@gmail.com>
2019-05-02 12:47:31 +0600

    improve response time

--- book-downloader.py
+++ book-downloader.py
@@ -3,48 +3,61 @@ import requests
 import wget
 import urllib
 import os
+import traceback
 from bs4 import BeautifulSoup
 
 # Set the URL you want to webscrape from
 url = 'https://doc.lagout.org/'
 
-validExtension = {'pdf','PDF','epub','EPUB','mobi','MOBI','flv','FLV',
-                    'awz3','AWZ3','chm','CHM','zip','ZIP','mp4','MP4',
-                    'xml','XML','jpg','jpeg','opf','png','PNG','djvu','DJVU'}
+def isDownloadable (response_content_type) :
+    return 'text/html' not in response_content_type
+
+@DeprecationWarning
+def getExtension(url):
+    return url.split('.')[-1]
 
 def generateFolder(dirName):
     try:
         if not os.path.exists(dirName):
             os.makedirs(dirName)
-    except Exception :
-        print("Error creating folder")
-
-def isDownloadable (fileExt) :
-    return fileExt in validExtension
-
-def getExtension(url):
-    return url.split('.')[-1]
+    except Exception:
+        print("\nError creating folder...")
+        traceback.print_exc() 
 
 def getDirectory(url):
-    splitedURL = url.split('/')
-    dirPathArr = splitedURL[3:-1]
-    dirPath = 'Books' #from current path
-    for i in range(0,len(dirPathArr)) :
-        dirPath += '/'+ dirPathArr[i]
+    try:
+        splitedURL = url.split('/')
+        dirPathArr = splitedURL[3:-1]
+        dirPath = 'Books' #from current path
+        for i in range(0,len(dirPathArr)) :
+            dirPath += '/'+ dirPathArr[i]
+    except Exception:
+        print("Directory name cereation error...")
+        traceback.print_exc()
+    finally:
+        dirPath = 'Books'
 
     generateFolder(dirPath)
     return dirPath
 
+def download(url,path):
+    try :
+        wget.download(url, path)
+        print("\nDownload Complete: "+ url)
+    except Exception:
+        print("\nError downloading file "+ url + " ...")
+        traceback.print_exc()
+
 def downloader(url):
-    fileExt = getExtension(url)
-    if isDownloadable(fileExt):
+    response = requests.head(url)
+    if response.status_code != 200 :
+        print('Request failed') 
+    elif isDownloadable(response.headers['Content-Type']) :
         decodedURL = urllib.parse.unquote(url)
         dirPath = getDirectory(decodedURL)
-        wget.download(decodedURL, dirPath)
-        print("\nDownload Complete: "+ decodedURL)
+        download(decodedURL,dirPath)
     else :
         response = requests.get(url)
-        #print(response.status_code)
         soup = BeautifulSoup(response.text, "html.parser")
         for i in range(1,len(soup.findAll('a'))):
             current_a = soup.findAll('a')[i]['href']
@@ -52,6 +65,6 @@ def downloader(url):
                 #print("BAD PARSING :" + current_a)
                 current_url  = url + current_a
                 #print("Recursive call :" + current_url)
-                downloader(current_url)
+                downloader(current_url) 
 
 downloader(url)
